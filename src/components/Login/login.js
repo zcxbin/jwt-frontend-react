90387224b378f093f65bc0d0305aa7213e065ef4
zcxbin <zcxbin@gmail.com>
2024-07-27 01:13:38 +0700

    add Navbar and route Login

---
+++ src/components/Login/login.js
@@ -0,0 +1,10 @@
+const Login = props => {
+  return (
+    <div className='login-container'>
+      Login
+      <button class='btn btn-primary'>test bootstrap</button>
+    </div>
+  )
+}
+
+export default Login

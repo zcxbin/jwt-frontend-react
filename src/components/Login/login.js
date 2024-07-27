5b21b822f97d0028db9f150dd80dc3343484c9aa
zcxbin <zcxbin@gmail.com>
2024-07-27 23:48:26 +0700

    Form Login Responsive

--- src/components/Login/login.js
+++ src/components/Login/login.js
@@ -2,16 +2,17 @@ import './login.scss'
 
 const Login = props => {
   return (
-    <div className='login-container mt-3'>
+    <div className='login-container'>
       <div className='container'>
-        <div className='row'>
-          <div className='content-left col-7'>
+        <div className='row px-3 px-sm-0'>
+          <div className='content-left col-12 d-none col-sm-7 d-sm-block'>
             <div className='branch'>ZCXBIN</div>
             <div className='detail'>
               ZCXBIN help you connect and share with the people in the world
             </div>
           </div>
-          <div className='content-right col-5 d-flex flex-column gap-3 py-3'>
+          <div className='content-right col-12 col-sm-5 d-flex flex-column gap-3 py-3'>
+            <div className='branch d-sm-none'>ZCXBIN</div>
             <input
               type='text'
               className='form-control'
@@ -23,7 +24,11 @@ const Login = props => {
               placeholder='Password'
             />
             <button className='btn btn-primary'>Login</button>
-            <span className='text-center'>Forgot your password?</span>
+            <span className='text-center'>
+              <a href='#' className='forgot-password'>
+                Forgot your password?
+              </a>
+            </span>
             <hr></hr>
             <div className='text-center'>
               <button className='btn btn-success'>Create new account</button>

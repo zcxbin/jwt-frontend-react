cdcb523619de29277d4659fb1b11ff52aa499def
zcxbin <zcxbin@gmail.com>
2024-07-29 13:59:01 +0700

    Form's State

--- src/components/Register/register.js
+++ src/components/Register/register.js
@@ -1,74 +1,104 @@
-import './register.scss'
-import { useHistory } from 'react-router-dom'
-import axios from 'axios'
-import { useEffect } from 'react'
-const Register = props => {
-  const history = useHistory()
-  const handleLogin = () => {
-    history.push('/login')
-  }
+import "./register.scss";
+import { useHistory } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
+const Register = (props) => {
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassWord] = useState("");
 
+  const history = useHistory();
+  const handleLogin = () => {
+    history.push("/login");
+  };
+
+  const handleRegister = () => {
+    let userData = {
+      email,
+      phone,
+      username,
+      password,
+    }
+    console.log(">>> check userData: ", userData)
+  };
   useEffect(() => {
-    axios.get("https://reqres.in/api/users?page=2").then(data => {
-      console.log(">> check data axios :", data)
-    })
+    // axios.get("http://localhost:8080/api/test-api").then(data => {
+    //   console.log(">> check data axios :", data)
+    // })
   }, []);
   return (
-    <div className='register-container'>
-      <div className='container'>
-        <div className='row px-3 px-sm-0'>
-          <div className='content-left col-12 d-none col-sm-7 d-sm-block'>
-            <div className='branch'>ZCXBIN</div>
-            <div className='detail'>
+    <div className="register-container">
+      <div className="container">
+        <div className="row px-3 px-sm-0">
+          <div className="content-left col-12 d-none col-sm-7 d-sm-block">
+            <div className="branch">ZCXBIN</div>
+            <div className="detail">
               ZCXBIN help you connect and share with the people in the world
             </div>
           </div>
-          <div className='content-right col-12 col-sm-5 d-flex flex-column gap-3 py-3'>
-            <div className='branch d-sm-none'>ZCXBIN</div>
-            <div className='form-group'>
+          <div className="content-right col-12 col-sm-5 d-flex flex-column gap-3 py-3">
+            <div className="branch d-sm-none">ZCXBIN</div>
+            <div className="form-group">
               <label>Email:</label>
               <input
-                type='text'
-                className='form-control'
-                placeholder='Email address'
+                type="text"
+                className="form-control"
+                placeholder="Email address"
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
               />
             </div>
-            <div className='form-group'>
+            <div className="form-group">
               <label>Phone Number:</label>
               <input
-                type='text'
-                className='form-control'
-                placeholder='Phone number'
+                type="text"
+                className="form-control"
+                placeholder="Phone number"
+                value={phone}
+                onChange={(event) => setPhone(event.target.value)}
               />
             </div>
-            <div className='form-group'>
+            <div className="form-group">
               <label>Username:</label>
               <input
-                type='text'
-                className='form-control'
-                placeholder='Username'
+                type="text"
+                className="form-control"
+                placeholder="Username"
+                value={username}
+                onChange={(event) => setUsername(event.target.value)}
               />
             </div>
-            <div className='form-group'>
+            <div className="form-group">
               <label>Password:</label>
               <input
-                type='password'
-                className='form-control'
-                placeholder='Password'
+                type="password"
+                className="form-control"
+                placeholder="Password"
+                value={password}
+                onChange={(event) => setPassword(event.target.value)}
               />
             </div>
-            <div className='form-group'>
+            <div className="form-group">
               <label>Re-enter Password:</label>
               <input
-                type='password'
-                className='form-control'
-                placeholder='Re-enter password'
+                type="password"
+                className="form-control"
+                placeholder="Re-enter password"
+                value={confirmPassword}
+                onChange={(event) => setConfirmPassWord(event.target.value)}
               />
             </div>
-            <button className='btn btn-primary'>Register</button>
+            <button
+              className="btn btn-primary"
+              onClick={() => handleRegister()}
+            >
+              Register
+            </button>
             <hr></hr>
-            <div className='text-center' onClick={() => handleLogin()}>
-              <button className='btn btn-success'>
+            <div className="text-center" onClick={() => handleLogin()}>
+              <button className="btn btn-success">
                 Already have an account. Login
               </button>
             </div>
@@ -76,7 +106,7 @@ const Register = props => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;

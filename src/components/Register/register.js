e4889c6a8b583cd5d77f389ca3e3a56e73f49866
zcxbin <zcxbin@gmail.com>
2024-08-08 17:18:56 +0700

    Register Complete

--- src/components/Register/register.js
+++ src/components/Register/register.js
@@ -4,7 +4,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
+import userService from '../../services/userService'
 const Register = (props) => {
   const [email, setEmail] = useState("");
   const [phone, setPhone] = useState("");
@@ -57,13 +57,20 @@ const Register = (props) => {
 
     return true;
   }
-  const handleRegister = () => {
+  const handleRegister = async () => {
 
     let check = isValid();
     if (check === true) {
-      axios.post("http://localhost:8080/api/v1/register", {
-        email, phone, username, password
-      })
+      let response = await userService.registerNewUser(email, phone, username, password);
+      let serverData = response.data;
+      if (+serverData.EC === 0) {
+        toast.success(serverData.EM);
+        history.push("/login");
+      }
+      else {
+        toast.error(serverData.EM);
+      }
+
 
     }
 
